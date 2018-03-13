66d46059eb159d061255b8519e6415a9903a9132
Chuang Cheng Heng <mukevin66@gmail.com>
2018-03-13 23:46:15 +0800

    FindWithTagCommandParser.java: Edit the implementation, the class now parser an arguments in the context of the FindWithTagCommand and returns a FindWIthTagCommand object for execution.

--- src/main/java/seedu/address/logic/parser/FindWithTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindWithTagCommandParser.java
@@ -4,29 +4,29 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 
 import java.util.Arrays;
 
-import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindWithTagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 /**
- * Parses input arguments and creates a new FindCommand object
+ * Parses input arguments and creates a new FindWithTagCommand object
  */
-public class FindWithTagCommandParser implements Parser<FindCommand> {
+public class FindWithTagCommandParser implements Parser<FindWithTagCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the FindCommand
-     * and returns an FindCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the FindWithTagCommand
+     * and returns an FindWithTagCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public FindCommand parse(String args) throws ParseException {
+    public FindWithTagCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindWithTagCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        String[] tagKeywords = trimmedArgs.split("\\s+");
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindWithTagCommand(new TagContainsKeywordsPredicate(Arrays.asList(tagKeywords)));
     }
 }
